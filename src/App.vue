7881b8137afaaa83999fa4fc0df8515ab038ac12
eric <howcrazys@gmail.com>
2021-06-14 23:41:19 +0800

    feat: add bootstrap +axios + vue axios

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,74 @@
       </router-link>
     </div>
     <router-view />
+    <button
+      type="button"
+      class="btn btn-primary"
+    >
+      Primary
+    </button>
+    <button
+      type="button"
+      class="btn btn-secondary"
+    >
+      Secondary
+    </button>
+    <button
+      type="button"
+      class="btn btn-success"
+    >
+      Success
+    </button>
+    <button
+      type="button"
+      class="btn btn-danger"
+    >
+      Danger
+    </button>
+    <button
+      type="button"
+      class="btn btn-warning"
+    >
+      Warning
+    </button>
+    <button
+      type="button"
+      class="btn btn-info"
+    >
+      Info
+    </button>
+    <button
+      type="button"
+      class="btn btn-light"
+    >
+      Light
+    </button>
+    <button
+      type="button"
+      class="btn btn-dark"
+    >
+      Dark
+    </button>
+
+    <button
+      type="button"
+      class="btn btn-link"
+    >
+      Link
+    </button>
   </div>
 </template>
 
+<script>
+export default {
+  created () {
+    console.dir(this.$http)
+  }
+}
+</script>
 <style lang="scss">
+@import "./assets/all.scss";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;

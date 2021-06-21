1378745bb5042e5abff7f89866ec1726aaa84f03
eric <howcrazys@gmail.com>
2021-06-21 23:20:04 +0800

    feat: swiper 測試

--- src/main.js
+++ src/main.js
@@ -5,6 +5,22 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import Loading from 'vue-loading-overlay'
 import 'vue-loading-overlay/dist/vue-loading.css'
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue'
+
+// Import Swiper styles
+import 'swiper/swiper.scss'
+
+import 'swiper/components/pagination/pagination.min.css'
+import 'swiper/components/navigation/navigation.min.css'
+
+// import Swiper core and required modules
+import SwiperCore, {
+  Pagination, Navigation
+} from 'swiper/core'
+
+// install Swiper modules
+SwiperCore.use([Pagination, Navigation])
 
 // 以上 npm 內容 以下自訂
 
@@ -19,5 +35,7 @@ app.use(VueAxios, axios)
 //   loader: 'dots'
 // })
 app.component('Loading', Loading)
+app.component('Swiper', Swiper)
+app.component('SwiperSlide', SwiperSlide)
 
 app.mount('#app')

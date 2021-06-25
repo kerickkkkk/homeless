b0b69fd0112c2921348316b55bef939430915a84
eric <howcrazys@gmail.com>
2021-06-25 23:50:41 +0800

    feat: login validation

--- src/views/dashboard/Login.vue
+++ src/views/dashboard/Login.vue
@@ -1,42 +1,63 @@
 <template>
   <div class="login">
+    <Loading
+      :active="isLoading"
+      color="#00BFFF"
+      loader="dots"
+    />
     <div class="contaienr">
       <div class="d-flex justify-content-center align-items-center vh-100">
-        <form id="formLogin">
+        <!-- values, validate -->
+        <Form
+          id="formLogin"
+          v-slot="{ errors}"
+          @submit="login"
+        >
           <div class="form-group">
             <h1 class="h3 text-center">
               請先登入
             </h1>
           </div>
           <div class="form-group">
-            <label for="username">Email address</label>
-            <input
-              id="username"
+            <label for="username">帳號</label>
+            <Field
+              id="email"
               v-model="user.username"
+              name="email"
               type="email"
               class="form-control"
-              name="username"
-              placeholder="email"
-            >
-          </div>
-          <div class="form-group">
-            <label for="password">Password</label>
-            <input
+              :class="{ 'is-invalid': errors['email'] }"
+              placeholder="請輸入 Email"
+              rules="email|required"
+            />
+            <ErrorMessage
+              name="email"
+              class="invalid-feedback"
+            />
+            <label for="username">密碼</label>
+            <Field
               id="password"
               v-model="user.password"
+              name="password"
               type="password"
               class="form-control"
-              placeholder="password"
+              :class="{ 'is-invalid': errors['password'] }"
+              placeholder="請輸入 password"
+              rules="required"
+            />
+            <ErrorMessage
+              name="password"
+              class="invalid-feedback"
+            />
+
+            <button
+              type="submit"
+              class="btn btn-primary btn-block"
             >
+              登入
+            </button>
           </div>
-          <button
-            type="button"
-            class="btn btn-primary btn-block"
-            @click="login"
-          >
-            登入
-          </button>
-        </form>
+        </Form>
       </div>
     </div>
   </div>
@@ -47,6 +68,7 @@ export default {
   name: 'Login',
   data () {
     return {
+      isLoading: false,
       user: {
         username: '',
         password: ''
@@ -55,7 +77,9 @@ export default {
   },
   methods: {
     login () {
+      this.isLoading = true
       this.$http.post(`${process.env.VUE_APP_API}/admin/signin`, this.user).then((res) => {
+        this.isLoading = false
         if (res.data.success) {
           const { expired, token } = res.data
           // 寫入 cookie
@@ -66,6 +90,7 @@ export default {
           alert(res.data.message)
         }
       }).catch((error) => {
+        this.isLoading = false
         console.log(error)
       })
     }

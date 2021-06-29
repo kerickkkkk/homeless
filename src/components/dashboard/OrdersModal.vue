c9a9cc18006471dab3d5bc49581a4cae94eb66a7
eric <howcrazys@gmail.com>
2021-06-29 23:33:05 +0800

    feat:  訂單功能 完成

---
+++ src/components/dashboard/OrdersModal.vue
@@ -0,0 +1,302 @@
+<template>
+  <div>
+    <!-- 檢視 -->
+    <div
+      ref="adminEditOrdersModal"
+      class="modal "
+      tabindex="-1"
+    >
+      <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">
+              訂單細節
+            </h5>
+            <button
+              type="button"
+              class="btn-close"
+              aria-label="Close"
+              @click="closeModal('edit')"
+            />
+          </div>
+          <div class="modal-body">
+            <div class="row">
+              <div class="col-md-4">
+                <h3>使用者資訊</h3>
+                <table class="table">
+                  <tbody v-if="tempOrder.user">
+                    <tr>
+                      <th width="80">
+                        姓名
+                      </th>
+                      <td>{{ tempOrder.user.name }}</td>
+                    </tr>
+                    <tr>
+                      <th>Email</th>
+                      <td>{{ tempOrder.user.email }}</td>
+                    </tr>
+                    <tr>
+                      <th>電話</th>
+                      <td>{{ tempOrder.user.tel }}</td>
+                    </tr>
+                    <tr>
+                      <th>地址</th>
+                      <td>{{ tempOrder.user.address }}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div class="col-md-8">
+                <h3>訂單資訊</h3>
+                <table class="table">
+                  <tbody v-if="tempOrder.user">
+                    <tr>
+                      <th>編號</th>
+                      <td>{{ tempOrder.id }}</td>
+                    </tr>
+                    <tr>
+                      <th width="100">
+                        下單時間
+                      </th>
+                      <td>{{ $filters.date(tempOrder.create_at) }}</td>
+                    </tr>
+                    <!-- <tr>
+                      <th>是否啟用</th>
+                      <td>
+                        {{ tempOrder.is_enabled ? '啟用' : '未啟用' }}
+                      </td>
+                    </tr> -->
+                    <tr>
+                      <th width="100">
+                        付款時間
+                      </th>
+                      <td>
+                        <span v-if="tempOrder.paid_date">
+                          {{ $filters.date(tempOrder.paid_date) }}
+                        </span>
+                        <span v-else>
+                          時間格式錯誤
+                        </span>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>是否付款</th>
+                      <td :class="[tempOrder.is_paid ? 'text-success': 'text-danger']">
+                        {{ tempOrder.is_paid ? '已付款' : '未付款' }}
+                      </td>
+                    </tr>
+                    <!-- <tr>
+                      <th>地址</th>
+                      <td>{{ tempOrder.user.address }}</td>
+                    </tr> -->
+                    <tr>
+                      <th>
+                        總價
+                      </th>
+                      <td>
+                        {{ `NT ${$filters.currency(tempOrder.total)}` }}
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <h3>訂購的商品</h3>
+                <table class="table">
+                  <tbody v-if="tempOrder.products">
+                    <tr
+                      v-for="product in tempOrder.products"
+                      :key="product.id"
+                    >
+                      <th>
+                        {{ product.product.title }}
+                      </th>
+                      <td width="100">
+                        {{ `${product.qty} / ${product.product.unit}` }}
+                      </td>
+                      <td
+                        width="100"
+                        class="text-end"
+                      >
+                        {{ `NT ${$filters.currency(product.final_total)}` }}
+                      </td>
+                    </tr>
+                  </tbody>
+                  <!-- <tfoot>
+                    <tr>
+                      <th
+                        colspan="2"
+                        class="text-end"
+                      >
+                        總價
+                      </th>
+                      <td class="text-end">
+                        {{ `NT ${$filters.currency(tempOrder.total)}` }}
+                      </td>
+                    </tr>
+                  </tfoot> -->
+                </table>
+                <div class="d-flex justify-content-end">
+                  <div class="form-check">
+                    <input
+                      id="payChecked"
+                      v-model="tempOrder.is_paid"
+                      class="form-check-input"
+                      type="checkbox"
+                    >
+                    <label
+                      class="form-check-label"
+                      for="payChecked"
+                    >
+                      {{ tempOrder.is_paid ? '已付款' : '未付款' }}
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-primary"
+              @click="changePayment(id)"
+            >
+              修改付款狀態並關閉
+            </button>
+            <button
+              type="button"
+              class="btn btn-outline-secondary"
+              @click="closeModal('edit')"
+            >
+              取消
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- 刪除 -->
+    <div
+      ref="adminDeleteOrderModal"
+      class="modal"
+      tabindex="-1"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">
+              刪除訂單
+            </h5>
+            <button
+              type="button"
+              class="btn-close"
+              aria-label="Close"
+              @click="closeModal('delete')"
+            />
+          </div>
+          <div class="modal-body">
+            <p class="h3">
+              是否要<span class="text-danger">刪除</span>??
+            </p>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-danger"
+              @click="deleteOrder(id)"
+            >
+              確定刪除
+            </button>
+            <button
+              type="button"
+              class="btn btn-outline-secondary"
+              @click="closeModal('delete')"
+            >
+              取消
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Modal } from 'bootstrap'
+export default {
+  data () {
+    return {
+      modal: {
+        delete: null,
+        edit: null
+      },
+      tempOrder: {},
+      id: null,
+      currentPage: 1
+    }
+  },
+  mounted () {
+    this.modal.delete = new Modal(this.$refs.adminDeleteOrderModal)
+    this.modal.edit = new Modal(this.$refs.adminEditOrdersModal)
+  },
+  methods: {
+    clearStatus () {
+      this.tempOrder = {}
+      this.id = null
+    },
+    openModal (type = 'delete', id, tempOrder, currentPage) {
+      if (type === 'edit') {
+        this.tempOrder = tempOrder
+      }
+      this.modal[type].show()
+      this.id = id
+      // 紀錄父層當下頁面 避免更新後跳回第一頁
+      this.current_page = currentPage
+      console.log('open', tempOrder.is_paid, tempOrder.id)
+    },
+    closeModal (type) {
+      this.modal[type].hide()
+      this.clearStatus()
+    },
+    deleteOrder (id) {
+      // /api/:api_path/admin/order/:order_id
+      this.$emitter.emit('fullScreenLoaidng', true)
+      const url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/order/${id}`
+      this.$http.delete(url)
+        .then((res) => {
+          this.$emitter.emit('fullScreenLoaidng', false)
+          if (res.data.success) {
+            this.closeModal('delete')
+            this.$swal('res.data.message', '', 'success').then(() => {
+              this.$emit('get-orders', this.current_page)
+            })
+            this.clearStatus()
+          } else {
+            this.$swal(res.data.message, '', 'error')
+          }
+        }).catch((error) => {
+          this.$emitter.emit('fullScreenLoaidng', false)
+          this.$swal(error, '', 'error')
+        })
+    },
+    changePayment (id) {
+      this.$emitter.emit('fullScreenLoaidng', true)
+      console.log(this.tempOrder.is_paid)
+      this.$http.put(`${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/order/${id}`, { data: this.tempOrder })
+        .then((res) => {
+          this.$emitter.emit('fullScreenLoaidng', false)
+          if (res.data.success) {
+            // 回來就要關掉 modal 資料狀態也可以清掉 不然 bs 會找不到
+            this.closeModal('edit')
+            this.$swal('付款狀態已修改', '', 'success').then(() => {
+              this.$emit('get-orders', this.current_page)
+            })
+          } else {
+            this.$swal(res.data.message, '', 'error')
+          }
+        }).catch((error) => {
+          this.$emitter.emit('fullScreenLoaidng', false)
+          this.$swal(error, '', 'error')
+        })
+    }
+  }
+}
+</script>
--- src/views/dashboard/Orders.vue
+++ src/views/dashboard/Orders.vue
@@ -1,17 +1,187 @@
 <template>
-  <div class="orders">
-    order
+  <div class="orders container my-3">
+    <table class="table mb-3 ">
+      <thead>
+        <tr>
+          <th width="100">
+            購買時間
+          </th>
+          <th width="100">
+            Email
+          </th>
+          <th>
+            購買款項
+          </th>
+          <th width="100">
+            應付金額
+          </th>
+          <th width="150">
+            是否付款
+          </th>
+          <th width="120">
+            編輯
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <template v-if="orders.length > 0">
+          <tr
+            v-for="order in orders"
+            :key="order.id"
+          >
+            <td>{{ order.create_at }}</td>
+            <td>
+              {{ order?.user.email }}
+            </td>
+            <td>
+              <ul>
+                <li
+                  v-for="product in order.products"
+                  :key="product.id"
+                >
+                  {{ `${product.product.title}: ${product.product.price} * ${product.qty} = ${product.final_total}` }}
+                </li>
+              </ul>
+            </td>
+            <td class="text-end">
+              ${{ order.total }}
+            </td>
+            <td>
+              <div class="form-check form-switch">
+                <input
+                  :id="`customSwitch${order.id}`"
+                  :checked="order.is_paid"
+                  class="form-check-input"
+                  type="checkbox"
+                  @change="changePayment(order.id ,order)"
+                >
+                <label
+                  class="form-check-label"
+                  :class="{'text-success': order.is_paid }"
+                  :for="`customSwitch${ order.id }`"
+                >{{ order.is_paid ? "已付款" : "未付款" }}</label>
+              </div>
+            </td>
+            <td>
+              <div
+                class="btn-group btn-group-sm"
+                role="group"
+                aria-label="Basic example"
+              >
+                <button
+                  type="button"
+                  class="btn btn-outline-primary"
+                  @click="modalHandler('edit', order.id ,{...order})"
+                >
+                  檢視
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-outline-danger"
+                  @click="modalHandler('delete', order.id)"
+                >
+                  刪除
+                </button>
+              </div>
+            </td>
+          </tr>
+        </template>
+        <tr v-else>
+          <td
+            class="text-center"
+            colspan="100"
+          >
+            目前沒有訂單唷!!!
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <Pagination
+      :pagination="pagination"
+      @get-orders="getOrders"
+    />
+    <adminOrdersModal
+      ref="adminOrdersModal"
+      @change-payment="changePayment"
+      @get-orders="getOrders"
+    />
   </div>
 </template>
 
 <script>
+import Pagination from '@/components/Pagination.vue'
+import adminOrdersModal from '@/components/dashboard/OrdersModal'
+
 export default {
   name: 'AdminOrders',
+  components: {
+    Pagination,
+    adminOrdersModal
+  },
   data () {
-    return {}
+    return {
+      orders: [],
+      pagination: {
+        category: null,
+        current_page: null,
+        has_next: false,
+        has_pre: false,
+        total_pages: null
+      }
+    }
+  },
+  created () {
+    this.getOrders()
   },
   methods: {
+    getOrders (page = 1) {
+      // /api/:api_path/admin/orders?page=:page
+      const url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/orders?page=${page}`
+      this.$emitter.emit('fullScreenLoaidng', true)
 
+      // this.$emitter.emit('toast:push', { icon: 'success', title: 'title' })
+
+      this.$http.get(url)
+        .then((res) => {
+          this.$emitter.emit('fullScreenLoaidng', false)
+          if (res.data.success) {
+            const { orders, pagination } = res.data
+            this.orders = orders
+            this.pagination = pagination
+          } else {
+            this.$swal(res.data.message, '', 'error')
+          }
+        }).catch((error) => {
+          this.$emitter.emit('fullScreenLoaidng', false)
+          this.$swal(error, '', 'error')
+        })
+    },
+    changePayment (id) {
+      // /api/:api_path/admin/order/:id
+      const index = this.orders.findIndex((product) => product.id === id)
+      this.orders[index].is_paid = !this.orders[index].is_paid
+
+      this.$emitter.emit('fullScreenLoaidng', true)
+      this.$http.put(`${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/order/${id}`, { data: this.orders[index] })
+        .then((res) => {
+          this.$emitter.emit('fullScreenLoaidng', false)
+          if (res.data.success) {
+            this.$swal('付款狀態已修改', '', 'success').then(() => {
+              this.getOrders()
+            })
+          } else {
+            this.$swal(res.data.message, '', 'error')
+          }
+        }).catch((error) => {
+          this.$emitter.emit('fullScreenLoaidng', false)
+          this.$swal(error, '', 'error')
+        })
+    },
+    // modal 控制
+    modalHandler (type, id, tempOrder = {}) {
+      // 控制往內部送
+      this.$refs.adminOrdersModal.openModal(type, id, tempOrder, this.pagination.current_page)
+    }
   }
 }
 </script>

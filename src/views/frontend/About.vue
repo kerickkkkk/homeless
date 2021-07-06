bef904be71af8c9656dc99e49a975150aa6daaf7
eric <howcrazys@gmail.com>
2021-07-06 22:35:36 +0800

    feat : about home 初步

--- src/views/frontend/About.vue
+++ src/views/frontend/About.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="about">
     <HeaderPic title="關於我們" />
+    <div class="container my-5">
+      <div class="row featurette align-items-center">
+        <div class="col-md-5  order-md-1">
+          <div
+            class="bg-cover"
+            style="background-image:url(https://images.unsplash.com/photo-1548705085-101177834f47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1189&q=80); height:300px"
+          />
+        </div>
+        <div class="col-md-7 order-0 order-md-1">
+          <div class="p-3">
+            <h2 class="featurette-heading">
+              逐食而居
+            </h2>
+            <p class="lead">
+              只要待著就會渾身不舒服，再某個轉彎點，毅然地跨出舒適圈選擇了，走到哪吃到哪？一台車，一群朋友，開啟了走到哪賣到哪，吃到哪的旅程，決心要吃爆，也以這種心情和顧客分享，每個地方的特色。
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <SubScribe />
   </div>
 </template>
 
 <script>
 import HeaderPic from '@/components/HeaderPic.vue'
+import SubScribe from '@/components/SubScribe.vue'
 export default {
   name: 'About',
   components: {
-    HeaderPic
+    HeaderPic,
+    SubScribe
   },
   data () {
     return {}
--- src/views/frontend/Index.vue
+++ src/views/frontend/Index.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div>
-    <swiper
+    <!-- <swiper
       :slides-per-view="1"
       :space-between="30"
       :loop="true"
@@ -17,28 +17,53 @@
           class="card-img-top"
           alt="..."
         >
-      </swiper-slide>  <swiper-slide>
-        <img
-          src="https://images.unsplash.com/photo-1624038030386-f3970b0c9c04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-          class="card-img-top"
-          alt="..."
-        >
       </swiper-slide>
-      <swiper-slide>
-        <img
-          src="https://images.unsplash.com/photo-1624038030386-f3970b0c9c04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-          class="card-img-top"
-          alt="..."
-        >
-      </swiper-slide>
-      <swiper-slide>
-        <img
-          src="https://images.unsplash.com/photo-1624038030386-f3970b0c9c04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-          class="card-img-top"
-          alt="..."
-        >
-      </swiper-slide>
-    </swiper>
+    </swiper> -->
+
+    <section class="about container my-4 ">
+      <div class="row featurette align-items-center">
+        <div class="col-md-7 order-1 order-md-0">
+          <div class="p-3">
+            <h2 class="featurette-heading">
+              原裝進口<span class="text-muted"> 取材絕不妥協</span>
+            </h2>
+            <p class="lead">
+              採純淨農場所栽種出的作物，經典農莊嚴格把關，創造出最重要的核心麵條。
+            </p>
+            <button class="btn btn-outline-primary">
+              關於我們 >>
+            </button>
+          </div>
+        </div>
+        <div class="col-md-5 oreder-0 order-md-1">
+          <div
+            class="bg-cover"
+            style="background-image:url(https://images.unsplash.com/photo-1499529112087-3cb3b73cec95?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80); height:300px"
+          />
+        </div>
+      </div>
+      <div class="row featurette align-items-center">
+        <div class="col-md-5  order-md-1">
+          <div
+            class="bg-cover"
+            style="background-image:url(https://images.unsplash.com/photo-1488459716781-31db52582fe9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80); height:300px"
+          />
+        </div>
+        <div class="col-md-7 order-0 order-md-1">
+          <div class="p-3">
+            <h2 class="featurette-heading">
+              產地鮮採<span class="text-muted"> 鮮多少買多少</span>
+            </h2>
+            <p class="lead">
+              絕不囤貨，當天訂購賣完為止，出餐多寡，全看鮮材多寡。
+            </p>
+            <button class="btn btn-outline-primary">
+              關於我們 >>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
     <SubScribe />
   </div>
 </template>

6b6164cc2b75ff5bd845eed2ed77c1c88456738b
eric <howcrazys@gmail.com>
2021-07-06 21:49:26 +0800

    fix : 購物車最小一後就不可點擊， 增加購物車為空狀態

--- src/views/frontend/Cart.vue
+++ src/views/frontend/Cart.vue
@@ -1,140 +1,159 @@
 <template>
   <div class="container">
+    <h2 class="text-center">
+      購物車內容
+    </h2>
     <div class="row">
-      <div class="text-end">
+      <!-- <div class="text-end">
         <button
           class="btn btn-sm btn-outline-danger"
           @click="deleteAllCart"
         >
           清空購物車
         </button>
-      </div>
-      <table class="table">
-        <thead>
-          <tr>
-            <th
-              width="50"
-              scope="col"
-            />
-            <th scope="col">
-              商品選項
-            </th>
-            <th scope="col">
-              單價
-            </th>
-            <th scope="col">
-              數量
-            </th>
-            <th scope="col">
-              小計
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="item in carts.carts"
-            :key="item.id"
-          >
-            <td>
-              <button
-                class="btn btn-outline-danger border-0"
-                @click="cartHandler('delete', item.id)"
-              >
-                <i class="bi bi-trash" />
-              </button>
-            </td>
-            <th>
-              <div class="cart__title d-flex">
-                <div class="me-3">
-                  <img
-                    :src="item.product.imageUrl"
-                    :alt="item.product.title"
-                    width="50"
-                    class="img-fluid"
-                  >
-                </div>
-                <div class="cart__title__content">
-                  <div class="h6">
-                    {{ item.product.title }}
+      </div> -->
+      <template v-if="cartLen > 0">
+        <table class="table">
+          <thead>
+            <tr>
+              <th
+                width="50"
+                scope="col"
+              />
+              <th scope="col">
+                商品選項
+              </th>
+              <th scope="col">
+                單價
+              </th>
+              <th scope="col">
+                數量
+              </th>
+              <th scope="col">
+                小計
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="item in carts.carts"
+              :key="item.id"
+            >
+              <td>
+                <button
+                  class="btn btn-outline-danger border-0"
+                  @click="cartHandler('delete', item.id)"
+                >
+                  <i class="bi bi-trash" />
+                </button>
+              </td>
+              <th>
+                <div class="cart__title d-flex">
+                  <div class="me-3">
+                    <img
+                      :src="item.product.imageUrl"
+                      :alt="item.product.title"
+                      width="50"
+                      class="img-fluid"
+                    >
+                  </div>
+                  <div class="cart__title__content">
+                    <div class="h6">
+                      {{ item.product.title }}
+                    </div>
                   </div>
                 </div>
-              </div>
-            </th>
-            <td class="text-end">
-              NT ${{ $filters.currency(item.product.price) }}
-            </td>
-            <td>
-              <div
-                class="btn-group"
-                role="group"
-                aria-label="Basic checkbox toggle button group"
-              >
-                <button
-                  :disabled="currentCartId === item.id"
-                  type="button"
-                  class="btn btn-outline-primary border-0"
-                  @click="cartHandler('put', item.id , item.product.id , --item.qty)"
+              </th>
+              <td class="text-end">
+                NT ${{ $filters.currency(item.product.price) }}
+              </td>
+              <td>
+                <div
+                  class="btn-group"
+                  role="group"
+                  aria-label="Basic checkbox toggle button group"
                 >
-                  －
-                </button>
-                <input
-                  v-model="item.qty"
-                  :disabled="currentCartId === item.id"
-                  type="number"
-                  min="1"
-                  class="form-control rounded-0"
-                  style="width: 80px"
-                  @change="cartHandler('put', item.id , item.product.id , ++item.qty)"
-                >
+                  <button
+                    :disabled="currentCartId === item.id || item.qty <2"
+                    type="button"
+                    class="btn btn-outline-primary border-0"
+                    @click="cartHandler('put', item.id , item.product.id , --item.qty)"
+                  >
+                    －
+                  </button>
+                  <input
+                    v-model="item.qty"
+                    :disabled="currentCartId === item.id"
+                    type="number"
+                    min="1"
+                    class="form-control rounded-0"
+                    style="width: 80px"
+                    @change="cartHandler('put', item.id , item.product.id , ++item.qty)"
+                  >
 
-                <button
-                  :disabled="currentCartId === item.id"
-                  type="button"
-                  class="btn btn-outline-primary border-0"
-                  @click="cartHandler('put', item.id , item.product.id , ++item.qty)"
-                >
-                  ＋
-                </button>
-              </div>
-            </td>
-            <td class="text-end">
-              NT ${{ $filters.currency(item.final_total) }}
-            </td>
-          </tr>
-        </tbody>
-        <tfoot>
-          <tr>
-            <td
-              class="text-end"
-              colspan="100"
-            >
-              原價 NT $ {{ $filters.currency(carts.total) }}
-            </td>
-          </tr>
-          <tr>
-            <td
-              class="text-end"
-              colspan="100"
-            >
-              總價 NT $ {{ $filters.currency( carts.final_total) }}
-            </td>
-          </tr>
-        </tfoot>
-      </table>
-      <div class="text-end">
+                  <button
+                    :disabled="currentCartId === item.id"
+                    type="button"
+                    class="btn btn-outline-primary border-0"
+                    @click="cartHandler('put', item.id , item.product.id , ++item.qty)"
+                  >
+                    ＋
+                  </button>
+                </div>
+              </td>
+              <td class="text-end">
+                NT ${{ $filters.currency(item.final_total) }}
+              </td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td
+                class="text-end"
+                colspan="100"
+              >
+                原價 NT $ {{ $filters.currency(carts.total) }}
+              </td>
+            </tr>
+            <tr>
+              <td
+                class="text-end"
+                colspan="100"
+              >
+                總價 NT $ {{ $filters.currency( carts.final_total) }}
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+        <div class="text-end">
+          <router-link
+            to="/products"
+            class="btn btn-outline-primary me-3"
+          >
+            繼續選購
+          </router-link>
+          <router-link
+            to="/checkout"
+            class="btn btn-danger"
+          >
+            結帳
+          </router-link>
+        </div>
+      </template>
+      <template v-else>
+        <h2 class="text-center">
+          購物車目前內容是空的
+        </h2>
+        <p class="h3 text-center">
+          快來去選產品吧
+        </p>
         <router-link
+          class="btn btn-primary"
           to="/products"
-          class="btn btn-outline-primary me-3"
         >
-          繼續選購
+          前往產品列表
         </router-link>
-        <router-link
-          to="/checkout"
-          class="btn btn-danger"
-        >
-          結帳
-        </router-link>
-      </div>
+      </template>
     </div>
   </div>
 </template>
@@ -163,7 +182,7 @@ export default {
           if (res.data.success) {
             const { data } = res.data
             this.carts = data
-            this.cartLen = data.length
+            this.cartLen = data.carts.length
           } else {
             this.$swal(res.data.message, '', 'error')
           }
@@ -185,7 +204,7 @@ export default {
       this.$http[type](`${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/cart/${id}`, { data })
         .then((res) => {
           this.$emitter.emit('fullScreenLoaidng', false)
-
+          this.$emitter.emit('nav-getCarts')
           this.currentCartId = null
           if (res.data.success) {
             this.$emitter.emit('toast:push', { icon: 'success', title: res.data.message })

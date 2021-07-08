d8f31100f973d4c0ce7ab37996bb22977c07e32f
eric <howcrazys@gmail.com>
2021-07-08 20:34:38 +0800

    fix :  修正 loading label 對應

--- src/views/dashboard/Login.vue
+++ src/views/dashboard/Login.vue
@@ -1,64 +1,57 @@
 <template>
-  <div class="login">
-    <Loading
-      :active="isLoading"
-      color="#00BFFF"
-      loader="dots"
-    />
-    <div class="contaienr">
-      <div class="d-flex justify-content-center align-items-center vh-100">
-        <!-- values, validate -->
-        <Form
-          id="formLogin"
-          v-slot="{ errors}"
-          @submit="login"
-        >
-          <div class="form-group">
-            <h1 class="h3 text-center">
-              請先登入
-            </h1>
-          </div>
-          <div class="form-group">
-            <label for="username">帳號</label>
-            <Field
-              id="email"
-              v-model="user.username"
-              name="email"
-              type="email"
-              class="form-control"
-              :class="{ 'is-invalid': errors['email'] }"
-              placeholder="請輸入 Email"
-              rules="email|required"
-            />
-            <ErrorMessage
-              name="email"
-              class="invalid-feedback"
-            />
-            <label for="username">密碼</label>
-            <Field
-              id="password"
-              v-model="user.password"
-              name="password"
-              type="password"
-              class="form-control"
-              :class="{ 'is-invalid': errors['password'] }"
-              placeholder="請輸入 password"
-              rules="required"
-            />
-            <ErrorMessage
-              name="password"
-              class="invalid-feedback"
-            />
+  <div class="contaienr">
+    <div class="d-flex justify-content-center align-items-center vh-100">
+      <!-- values, validate -->
+      <Form
+        id="formLogin"
+        v-slot="{ errors}"
+        @submit="login"
+      >
+        <div class="form-group">
+          <h1 class="h3 text-center">
+            請先登入
+          </h1>
+        </div>
+        <div class="form-group">
+          <label for="email">帳號</label>
+          <Field
+            id="email"
+            v-model="user.username"
+            name="email"
+            type="email"
+            class="form-control"
+            :class="{ 'is-invalid': errors['email'] }"
+            placeholder="請輸入 Email"
+            rules="email|required"
+          />
+          <ErrorMessage
+            name="email"
+            class="invalid-feedback"
+          />
+          <label for="password">密碼</label>
+          <Field
+            id="password"
+            v-model="user.password"
+            name="password"
+            type="password"
+            class="form-control"
+            :class="{ 'is-invalid': errors['password'] }"
+            placeholder="請輸入 password"
+            rules="required"
+          />
+          <ErrorMessage
+            name="password"
+            class="invalid-feedback"
+          />
 
-            <button
-              type="submit"
-              class="btn btn-primary btn-block"
-            >
-              登入
-            </button>
-          </div>
-        </Form>
-      </div>
+          <button
+            type="submit"
+            class="btn btn-primary btn-block"
+          >
+            登入
+          </button>
+        </div>
+      </Form>
     </div>
   </div>
 </template>
@@ -68,7 +61,6 @@ export default {
   name: 'Login',
   data () {
     return {
-      isLoading: false,
       user: {
         username: '',
         password: ''
@@ -77,21 +69,20 @@ export default {
   },
   methods: {
     login () {
-      this.isLoading = true
+      this.$emitter.emit('fullScreenLoaidng', true)
       this.$http.post(`${process.env.VUE_APP_API}/admin/signin`, this.user).then((res) => {
-        this.isLoading = false
+        this.$emitter.emit('fullScreenLoaidng', false)
         if (res.data.success) {
           const { expired, token } = res.data
           // 寫入 cookie
           document.cookie = `shop=${token}; expires=${new Date(expired)}`
-          console.log(res.data)
           this.$router.push('/admin')
         } else {
-          alert(res.data.message)
+          this.$swal(res.data.message, '', 'error')
         }
       }).catch((error) => {
-        this.isLoading = false
-        console.log(error)
+        this.$emitter.emit('fullScreenLoaidng', false)
+        this.$swal(error, '', 'error')
       })
     }
   }
